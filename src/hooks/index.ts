488bc973ce1ac5e87dd45978383c784d47ccaf72
Kacper Grzeszczyk <kacgrzes@gmail.com>
2021-09-21 23:01:42 +0200

    Reexport hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,21 @@
 // Reexport all the hooks that you use within the project here
 
 export { useTranslation } from 'react-i18next'
-export { useNavigationContainerRef } from '@react-navigation/native'
+export {
+  useNavigationContainerRef,
+  useFocusEffect,
+  useIsFocused,
+  useLinkBuilder,
+  useLinkProps,
+  useLinkTo,
+  useNavigation,
+  useNavigationBuilder,
+  useNavigationState,
+  useRoute,
+  useScrollToTop,
+  useTheme,
+} from '@react-navigation/native'
+export * from '@react-native-community/hooks'
 export * from './useAuth'
 export * from './useCachedResources'
 export * from './useColorScheme'
