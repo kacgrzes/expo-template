30fd2690b0f1a0bd3bf04b017d919b97eacc4ea3
Kacper Grzeszczyk <kacgrzes@gmail.com>
2021-10-06 17:55:04 +0200

    Add icons to BottomTabNavigator

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,13 +1,37 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
+import { createBottomTabNavigator, BottomTabNavigationOptions } from '@react-navigation/bottom-tabs'
 import React, { FC } from 'react'
 
+import { useCallback } from '~hooks'
 import { ExamplesScreen, HomeScreen } from '~screens'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export const BottomTabNavigator: FC = () => {
+  const screenOptions = useCallback(
+    ({ route }): BottomTabNavigationOptions => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName: keyof typeof Feather.glyphMap
+
+        if (route.name === 'Home') {
+          iconName = 'home'
+        } else if (route.name === 'Examples') {
+          iconName = 'list'
+        } else {
+          iconName = 'alert-triangle'
+        }
+
+        // You can return any component that you like here!
+        return <Feather name={iconName} size={size} color={color} />
+      },
+      tabBarActiveTintColor: 'black',
+      tabBarInactiveTintColor: 'gray',
+    }),
+    []
+  )
+
   return (
-    <Navigator>
+    <Navigator screenOptions={screenOptions}>
       <Screen name="Home" component={HomeScreen} />
       <Screen name="Examples" component={ExamplesScreen} />
     </Navigator>
