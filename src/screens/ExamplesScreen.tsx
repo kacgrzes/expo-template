c0ed3aff6ae01ac70bf277053ef08a8395ca6595
Łukasz Patalan <lukasz.patalan@gmail.com>
2022-01-24 18:09:07 +0100

    implement react native svg

--- src/screens/ExamplesScreen.tsx
+++ src/screens/ExamplesScreen.tsx
@@ -1,5 +1,6 @@
+import { CheckIcon } from 'assets/icons/CheckIcon'
 import { useCallback } from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, View } from 'react-native'
 
 import { Button } from '~components'
 import { useTheme } from '~hooks'
@@ -8,7 +9,7 @@ export const ExamplesScreen = (props: ExamplesScreenProps): JSX.Element => {
   const {
     navigation: { navigate },
   } = props
-  const { s } = useTheme()
+  const { s, colors } = useTheme()
 
   const goToApplicationInfo = useCallback(() => navigate('ApplicationInfo'), [navigate])
   const goToAppSettings = useCallback(() => navigate('Settings'), [navigate])
@@ -21,9 +22,14 @@ export const ExamplesScreen = (props: ExamplesScreenProps): JSX.Element => {
       }),
     [navigate]
   )
-
+  const exampleColors = [colors.error, colors.warningLight, colors.focused]
   return (
     <ScrollView contentContainerStyle={[s.flex1, s.itemsCenter, s.justifyCenter]}>
+      <View style={[s.flexRow, s.mB10]}>
+        {exampleColors.map((color) => (
+          <CheckIcon key={color} color={color} />
+        ))}
+      </View>
       <Button style={s.mB2} onPress={goToApplicationInfo} title="Go to ApplicationInfo" />
       <Button style={s.mB2} onPress={goToComponents} title="Go to Components" />
       <Button style={s.mB2} onPress={goToHomeStackDetails} title="Go to Home Stack Details" />
