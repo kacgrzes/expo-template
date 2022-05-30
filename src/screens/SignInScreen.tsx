3f7e4d13de142e62eba2fc54f9659a6d8dc3d7df
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2022-05-30 18:02:31 +0200

    Redesign signIn screen

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,15 +1,61 @@
-import { View } from 'react-native'
+import { TextInput } from 'react-native'
 
-import { Button } from '~components'
-import { useAuth, useTheme } from '~hooks'
+import { Box, Button, Center, ControlledInput, Text } from '~components'
+import { REGEX } from '~constants'
+import { useRef, useSignInForm, useTheme, useTranslation } from '~hooks'
 
 export const SignInScreen = (): JSX.Element => {
-  const { signIn } = useAuth()
   const { s } = useTheme()
+  const { t } = useTranslation()
+  const emailInputRef = useRef<TextInput>(null)
+
+  const { control, error, errors, submit, isSubmitting } = useSignInForm()
 
   return (
-    <View style={[s.flex1, s.justifyCenter, s.itemsCenter]}>
-      <Button onPress={signIn} title="Sign in" />
-    </View>
+    <Center px={8} flex={1} bg="gray100">
+      <ControlledInput
+        fullWidth
+        label={t('common.email_label')}
+        control={control}
+        errors={errors}
+        keyboardType="email-address"
+        placeholder={t('common.email_placeholder')}
+        name="email"
+        type="email"
+        returnKeyType="next"
+        onSubmitEditing={emailInputRef.current?.focus}
+        rules={{
+          required: t('form.required'),
+          pattern: {
+            value: REGEX.EMAIL,
+            message: t('form.invalid_email_format'),
+          },
+        }}
+        containerStyle={[s.mB2]}
+      />
+      <ControlledInput
+        ref={emailInputRef}
+        fullWidth
+        label={t('sign_in_screen.password_label')}
+        control={control}
+        errors={errors}
+        placeholder={t('sign_in_screen.password_placeholder')}
+        name="password"
+        type="protected"
+        returnKeyType="send"
+        onSubmitEditing={submit}
+        rules={{ required: t('form.required') }}
+        containerStyle={[s.mB2]}
+      />
+      <Box mt={4} />
+      <Button onPress={submit} title="Sign in" loading={isSubmitting} disabled={isSubmitting} />
+      <Box mt={4} />
+      {!!error && <Text.BodyBold color="errorLight">{error}</Text.BodyBold>}
+      {/* TODO: Remove this after implementing signing in with backend  */}
+      <Text.BodyBold>Correct credentials</Text.BodyBold>
+      <Text.BodyRegular color="gray500" center>
+        Email: test@example.com{'\n'}Password: 123456
+      </Text.BodyRegular>
+    </Center>
   )
 }
