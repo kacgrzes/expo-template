550a3bd1f3ec0e1d39a03d661ecf1f02bbda728d
Michał Baumruck <m.baumruck@binarapps.com>
2022-10-27 15:24:16 +0200

    fix require cycle warning

--- src/hooks/forms/useSignInForm.ts
+++ src/hooks/forms/useSignInForm.ts
@@ -4,7 +4,8 @@ import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { isError } from 'react-query'
 
-import { useAuth } from '~hooks'
+import { useAuth } from '../useAuth'
+
 import { SignInFormValues } from '~types/authForms'
 
 const defaultValues: SignInFormValues = {
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,20 @@
 // Reexport all the hooks that you use within the project here
-
+// Hooks from external packages
+export {
+  useBottomSheet,
+  useBottomSheetDynamicSnapPoints,
+  useBottomSheetInternal,
+  useBottomSheetModal,
+  useBottomSheetSpringConfigs,
+  useBottomSheetTimingConfigs,
+} from '@gorhom/bottom-sheet'
+export { useAsyncStorage } from '@react-native-async-storage/async-storage'
+export * from '@react-native-community/hooks'
+export { useNetInfo } from '@react-native-community/netinfo'
+export { useAssets } from 'expo-asset'
+export { useFonts } from 'expo-font'
+export { useURL } from 'expo-linking'
+export { useAnimationState, useDynamicAnimation } from 'moti'
 export {
   useAccessibleColors,
   useColorMode,
@@ -21,29 +36,6 @@ export {
   useState,
 } from 'react'
 export {
-  useBottomSheet,
-  useBottomSheetDynamicSnapPoints,
-  useBottomSheetInternal,
-  useBottomSheetModal,
-  useBottomSheetSpringConfigs,
-  useBottomSheetTimingConfigs,
-} from '@gorhom/bottom-sheet'
-export { useAsyncStorage } from '@react-native-async-storage/async-storage'
-export { useNetInfo } from '@react-native-community/netinfo'
-export { useAssets } from 'expo-asset'
-export { useFonts } from 'expo-font'
-export { useURL } from 'expo-linking'
-export { useAnimationState, useDynamicAnimation } from 'moti'
-export { useSSR, useTranslation } from 'react-i18next'
-export { useWindowDimensions } from 'react-native'
-export {
-  useSpring,
-  useTiming,
-  useTranslation as useTranslationRedash,
-  useVector,
-} from 'react-native-redash'
-export { useSafeAreaFrame, useSafeAreaInsets } from 'react-native-safe-area-context'
-export {
   useController,
   useFieldArray,
   useForm,
@@ -51,7 +43,16 @@ export {
   useFormState,
   useWatch,
 } from 'react-hook-form'
-export * from '@react-native-community/hooks'
+export { useSSR, useTranslation } from 'react-i18next'
+export { useWindowDimensions } from 'react-native'
+export { useNotificationController, useNotifications } from 'react-native-notificated'
+export {
+  useSpring,
+  useTiming,
+  useTranslation as useTranslationRedash,
+  useVector,
+} from 'react-native-redash'
+export { useSafeAreaFrame, useSafeAreaInsets } from 'react-native-safe-area-context'
 export {
   useHydrate,
   useInfiniteQuery,
@@ -63,14 +64,17 @@ export {
   useQueryClient,
   useQueryErrorResetBoundary,
 } from 'react-query'
+export { useDebounce, useDebouncedCallback, useThrottledCallback } from 'use-debounce'
+
+// Custom hooks implemented in app
+
 export * from './forms'
 export * from './navigation'
+
 export * from './useAppStateActive'
+export * from './useAuth'
+export * from './useBoolean'
 export * from './useCachedResources'
 export * from './useSecurePassword'
-export * from './useToggle'
-export * from './useBoolean'
-export { useDebounce, useDebouncedCallback, useThrottledCallback } from 'use-debounce'
 export * from './useTimestamp'
-export { useNotificationController, useNotifications } from 'react-native-notificated'
-export { useAuthContext as useAuth } from '~contexts'
+export * from './useToggle'
