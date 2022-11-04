0f3c5dddd9596141dba80644063710c64115e8cd
Jakub Zagórski <70406931+zagoorland@users.noreply.github.com>
2022-11-04 11:25:23 +0100

    added alert working on web

--- src/hooks/navigation/usePreventGoBack.ts
+++ src/hooks/navigation/usePreventGoBack.ts
@@ -1,11 +1,26 @@
 import { useNavigation } from '@react-navigation/native'
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Alert } from 'react-native'
+import { Alert, Platform } from 'react-native'
 
 export const usePreventGoBack = (shouldPrevent = true): void => {
   const { t } = useTranslation()
   const navigation = useNavigation()
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const alertPolyfill = (title: string, description: string, options: any) => {
+    const result = window.confirm([title, description].filter(Boolean).join('\n'))
+
+    if (result) {
+      const confirmOption = options.find(({ style }: { style: string }) => style !== 'cancel')
+      confirmOption && confirmOption.onPress()
+    } else {
+      const cancelOption = options.find(({ style }: { style: string }) => style === 'cancel')
+      cancelOption && cancelOption.onPress()
+    }
+  }
+
+  const alert = Platform.OS === 'web' ? alertPolyfill : Alert.alert
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -16,7 +31,7 @@ export const usePreventGoBack = (shouldPrevent = true): void => {
 
       event?.preventDefault()
 
-      Alert.alert(
+      alert(
         t('navigation.prevent_go_back_alert.title'),
         t('navigation.prevent_go_back_alert.description'),
         [
@@ -37,5 +52,5 @@ export const usePreventGoBack = (shouldPrevent = true): void => {
     navigation.addListener('beforeRemove', callback)
 
     return () => navigation.removeListener('beforeRemove', callback)
-  }, [navigation, shouldPrevent, t])
+  }, [navigation, shouldPrevent, t, alert])
 }
