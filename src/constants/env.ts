1231da7b6ccf120e8cf8788025a303a0171093e9
Michał Baumruck <m.baumruck@binarapps.com>
2023-01-13 09:24:42 +0100

    improve build flow settings

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,6 @@
-//CONFIG: This is example URL, change to proper
+import Constants from 'expo-constants'
 
-export const API_URL = 'https://catfact.ninja'
+export const ENV = {
+  API_URL: 'https://catfact.ninja', //CONFIG: This is example URL, change to proper,  TODO: create env for dev/staging/prod and keep it there
+  ENVIRONMENT: Constants?.expoConfig?.extra?.ENVIRONMENT,
+}
