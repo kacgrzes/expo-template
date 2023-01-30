779de9c5ec4393055eb40e52644c6e42b026e6e9
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2023-01-30 15:12:13 +0100

    chore: remove react-native-keyboard-aware-scroll-view package

--- jest.setup.js
+++ jest.setup.js
@@ -4,11 +4,6 @@ jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 
-jest.mock('react-native-keyboard-aware-scroll-view', () => {
-  const KeyboardAwareScrollView = ({ children }) => children
-  return { KeyboardAwareScrollView }
-})
-
 // Workaround copied from the reanimated contributor github comment
 // Src: https://github.com/software-mansion/react-native-reanimated/issues/3125#issuecomment-1085865635
 global.__reanimatedWorkletInit = () => ({})
