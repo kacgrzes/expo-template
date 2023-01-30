1943d8ef712b2ce707650b93e30983d5186c1d3c
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2023-01-30 14:32:34 +0100

    fix:  improve keyboard scroll component

--- src/components/KeyboardAwareScrollView.tsx
+++ src/components/KeyboardAwareScrollView.tsx
@@ -1,52 +1,77 @@
-import { ScrollView } from 'native-base'
-import { useMemo } from 'react'
-import { Platform, StyleProp, StyleSheet, ViewStyle } from 'react-native'
-import {
-  KeyboardAwareScrollView as KeyboardAwareScroll,
-  KeyboardAwareScrollViewProps,
-} from 'react-native-keyboard-aware-scroll-view'
+import { KeyboardAvoidingView, View, ScrollView, IScrollViewProps } from 'native-base'
+import React, { FC } from 'react'
+import { KeyboardAvoidingViewProps, Platform, StyleProp, ViewStyle } from 'react-native'
+import { Edge } from 'react-native-safe-area-context'
 
-import { useNavigationTheme } from '~hooks'
+import { useSafeAreaInsetsStyle } from '~hooks'
 
-const keyboardDismissMode = Platform.OS === 'android' ? 'none' : 'interactive'
-const ScrollWrapper = Platform.OS === 'android' ? ScrollView : KeyboardAwareScroll
-
-type Props = Omit<KeyboardAwareScrollViewProps, 'contentContainerStyle'> & {
+type Props = {
+  /**
+   * Should keyboard persist on screen tap. Defaults to handled.
+   */
+  keyboardShouldPersistTaps?: 'handled' | 'always' | 'never'
+  /**
+   * Pass any additional props directly to the ScrollView component.
+   */
+  ScrollViewProps?: IScrollViewProps
+  /**
+   * Children components.
+   */
+  children?: React.ReactNode
+  /**
+   * Style for the outer content container useful for padding & margin.
+   */
+  style?: StyleProp<ViewStyle>
+  /**
+   * Style for the inner content container useful for padding & margin.
+   */
   contentContainerStyle?: StyleProp<ViewStyle>
+  /**
+   * Override the default edges for the safe area.
+   */
+  safeAreaEdges?: Edge[]
+  /**
+   * Pass any additional props directly to the KeyboardAvoidingView component.
+   */
+  KeyboardAvoidingViewProps?: KeyboardAvoidingViewProps
 }
 
-export const KeyboardAwareScrollView = ({
-  children,
-  contentContainerStyle = {},
-  ...rest
-}: Props): JSX.Element => {
-  const { navigationTheme } = useNavigationTheme()
-  const scrollViewContentContainerStyle = useMemo(
-    () =>
-      StyleSheet.compose<ViewStyle>(
-        [
-          {
-            alignItems: 'center',
-            backgroundColor: navigationTheme.colors.background,
-            flexGrow: 1,
-            justifyContent: 'space-between',
-          },
-        ],
-        contentContainerStyle
-      ),
-    [contentContainerStyle, navigationTheme.colors.background]
-  )
+const isIos = Platform.OS === 'ios'
+
+export const KeyboardAwareScrollView: FC<Props> = (props) => {
+  const {
+    KeyboardAvoidingViewProps,
+    safeAreaEdges = [],
+    children,
+    keyboardShouldPersistTaps = 'handled',
+    contentContainerStyle,
+    ScrollViewProps,
+    style,
+  } = props
+
+  const $containerInsets = useSafeAreaInsetsStyle(safeAreaEdges)
+
   return (
-    <ScrollWrapper
-      contentContainerStyle={scrollViewContentContainerStyle}
-      enableAutomaticScroll
-      keyboardDismissMode={keyboardDismissMode}
-      keyboardOpeningTime={0}
-      keyboardShouldPersistTaps="handled"
-      showsVerticalScrollIndicator={false}
-      {...rest}
-    >
-      {children}
-    </ScrollWrapper>
+    <View style={[$flexStyle, $containerInsets]}>
+      <KeyboardAvoidingView
+        behavior={isIos ? 'padding' : undefined}
+        keyboardVerticalOffset={100}
+        {...KeyboardAvoidingViewProps}
+        style={[$flexStyle, KeyboardAvoidingViewProps?.style]}
+      >
+        <ScrollView
+          {...ScrollViewProps}
+          keyboardShouldPersistTaps={keyboardShouldPersistTaps}
+          style={[$flexStyle, ScrollViewProps?.style, style]}
+          contentContainerStyle={[ScrollViewProps?.contentContainerStyle, contentContainerStyle]}
+        >
+          {children}
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
   )
+}
+
+const $flexStyle: ViewStyle = {
+  flex: 1,
 }
