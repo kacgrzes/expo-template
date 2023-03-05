d9118815b2870c49844c945ea772b541acdcaf90
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2023-03-05 02:12:01 +0100

    fix: imporve scroll behavior for screens with keyboard

--- src/components/KeyboardAwareScrollView.tsx
+++ src/components/KeyboardAwareScrollView.tsx
@@ -1,91 +1,53 @@
-import { KeyboardAvoidingView, View, ScrollView, IScrollViewProps } from 'native-base'
-import React, { FC, useMemo } from 'react'
-import { KeyboardAvoidingViewProps, Platform, StyleProp, StyleSheet, ViewStyle } from 'react-native'
-import { Edge } from 'react-native-safe-area-context'
+import { forwardRef, useMemo } from 'react'
+import { Platform, StyleProp, StyleSheet, ViewStyle } from 'react-native'
+import {
+  KeyboardAwareScrollView as KeyboardAwareScroll,
+  KeyboardAwareScrollViewProps,
+} from 'react-native-keyboard-aware-scroll-view'
 
-import { useNavigationTheme, useSafeAreaInsetsStyle } from '~hooks'
+import { useNavigationTheme } from '~hooks'
 
-type Props = {
-  /**
-   * Should keyboard persist on screen tap. Defaults to handled.
-   */
-  keyboardShouldPersistTaps?: 'handled' | 'always' | 'never'
-  /**
-   * Pass any additional props directly to the ScrollView component.
-   */
-  ScrollViewProps?: IScrollViewProps
-  /**
-   * Children components.
-   */
-  children?: React.ReactNode
-  /**
-   * Style for the outer content container useful for padding & margin.
-   */
-  style?: StyleProp<ViewStyle>
-  /**
-   * Style for the inner content container useful for padding & margin.
-   */
-  contentContainerStyle?: StyleProp<ViewStyle>
-  /**
-   * Override the default edges for the safe area.
-   */
-  safeAreaEdges?: Edge[]
-  /**
-   * Pass any additional props directly to the KeyboardAvoidingView component.
-   */
-  KeyboardAvoidingViewProps?: KeyboardAvoidingViewProps
+const keyboardDismissMode = Platform.OS === 'android' ? 'on-drag' : 'interactive'
+
+type Props = Omit<KeyboardAwareScrollViewProps, 'contentContainerStyle'> & {
+  contentContainerStyle?: Omit<StyleProp<ViewStyle>, 'false'>
 }
 
-const isIos = Platform.OS === 'ios'
+export const KeyboardAwareScrollView = forwardRef<KeyboardAwareScroll, Props>(
+  ({ children, onMomentumScrollEnd, contentContainerStyle = {}, ...rest }, ref) => {
+    const { navigationTheme } = useNavigationTheme()
 
-export const KeyboardAwareScrollView: FC<Props> = (props) => {
-  const {
-    KeyboardAvoidingViewProps,
-    safeAreaEdges = [],
-    children,
-    keyboardShouldPersistTaps = 'handled',
-    ScrollViewProps,
-  } = props
+    const scrollViewContentContainerStyle = useMemo(
+      () =>
+        StyleSheet.compose<ViewStyle>(
+          [
+            {
+              alignItems: 'center',
+              backgroundColor: navigationTheme.colors.background,
+              flexGrow: 1,
+              justifyContent: 'space-between',
+            },
+          ],
+          contentContainerStyle
+        ),
+      [contentContainerStyle, navigationTheme.colors.background]
+    )
 
-  const { navigationTheme } = useNavigationTheme()
-
-  const scrollViewContentContainerStyle = useMemo(
-    () =>
-      StyleSheet.compose<ViewStyle>(
-        [
-          {
-            alignItems: 'center',
-            backgroundColor: navigationTheme.colors.background,
-            flexGrow: 1,
-            justifyContent: 'space-between',
-          },
-        ],
-        ScrollViewProps?.contentContainerStyle
-      ),
-    [ScrollViewProps?.contentContainerStyle, navigationTheme.colors.background]
-  )
-
-  const $containerInsets = useSafeAreaInsetsStyle(safeAreaEdges)
-
-  return (
-    <View style={[$flexStyle, $containerInsets]}>
-      <KeyboardAvoidingView
-        behavior={isIos ? 'padding' : undefined}
-        keyboardVerticalOffset={100}
-        {...KeyboardAvoidingViewProps}
+    return (
+      <KeyboardAwareScroll
+        ref={ref}
+        enableOnAndroid
+        bounces={false}
+        alwaysBounceHorizontal={false}
+        enableResetScrollToCoords={false}
+        alwaysBounceVertical={false}
+        keyboardShouldPersistTaps="handled"
+        keyboardDismissMode={keyboardDismissMode}
+        contentContainerStyle={scrollViewContentContainerStyle}
+        {...rest}
       >
-        <ScrollView
-          {...ScrollViewProps}
-          keyboardShouldPersistTaps={keyboardShouldPersistTaps}
-          contentContainerStyle={scrollViewContentContainerStyle}
-        >
-          {children}
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </View>
-  )
-}
-
-const $flexStyle: ViewStyle = {
-  flex: 1,
-}
+        {children}
+      </KeyboardAwareScroll>
+    )
+  }
+)
