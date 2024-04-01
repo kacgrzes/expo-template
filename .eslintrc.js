f924c502b3377949716993d72eb9fa4ded6c99da
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-01 11:55:13 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["universe/native"],
+  rules: {
+    // Ensures props and state inside functions are always up-to-date
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
