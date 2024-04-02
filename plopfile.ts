5d0b78e7478d322a3d9cb86d2ee48e69bd3c8a00
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-02 11:57:58 +0200

    Add example plop file

---
+++ plopfile.ts
@@ -0,0 +1,24 @@
+// plopfile.ts
+import { NodePlopAPI } from "plop";
+
+export default function (plop: NodePlopAPI) {
+  // plop generator code
+  plop.setWelcomeMessage("Welcome to the plop generator");
+  plop.setGenerator("component", {
+    description: "all the files related to React UI components",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "component name please",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "components/{{name}}.tsx",
+        templateFile: "plop-templates/controller.hbs",
+      },
+    ],
+  });
+}
