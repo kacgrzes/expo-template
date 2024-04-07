ce67f8fda899a4840c0fc78aa1cb24c9de9c2051
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-07 22:32:36 +0200

    Enable freeze

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import "../unistyles";
 
 import { Slot } from "expo-router";
+import { enableFreeze } from "react-native-screens";
 
 import { useOrientationLock } from "../hooks/useOrientationLock";
 import { useQuickActionSetup } from "../hooks/useQuickActionSetup";
+
+enableFreeze(true);
 
 export default function () {
   useOrientationLock();
