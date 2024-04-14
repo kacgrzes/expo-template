1e899294a5840cc1460bffaa13dc87e344dd57e5
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-14 21:57:46 +0200

    Customize metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
