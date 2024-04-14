a34d2bb24a5eeddebb204a40db09c97a5ad4b43e
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-14 21:20:32 +0200

    Setup testing using jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "jest-expo",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  verbose: true,
+};
+
+export default config;
---
+++ jest.setup.ts
@@ -0,0 +1,2 @@
+import "@testing-library/jest-native/extend-expect";
+import "@testing-library/react-native/extend-expect";
