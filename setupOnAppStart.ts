16df1b7b8850751b14d6bb3abe0a35e4ea6c5419
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-15 22:02:36 +0200

    Add LogBox.ignoreLogs

--- setupOnAppStart.ts
+++ setupOnAppStart.ts
@@ -1,7 +1,9 @@
 import { preventAutoHideAsync } from "expo-splash-screen";
+import { LogBox } from "react-native";
 import { enableFreeze } from "react-native-screens";
 
 export const setupOnAppStart = () => {
   preventAutoHideAsync();
   enableFreeze(true);
+  LogBox.ignoreLogs([]);
 };
