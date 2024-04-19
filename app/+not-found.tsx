4f61f6421708d13b4739c0ba90bc6de263649cf4
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-19 12:19:03 +0200

    Add NotFound screen

---
+++ app/+not-found.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
+import { isDeveloopment } from "utils/env";
+
+export default function NotFound() {
+  const { styles } = useStyles(stylesheet);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Not found!</Text>
+      <Link href={"/"}>
+        <Text>Go back home</Text>
+      </Link>
+      {isDeveloopment && (
+        <Link href="_sitemap">
+          <Text>Sitemap</Text>
+        </Link>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const stylesheet = createStyleSheet({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
