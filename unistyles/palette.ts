4390e432b269cdc9712ed6a45a35211520c6e650
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-20 13:32:06 +0200

    Create color palette

---
+++ unistyles/palette.ts
@@ -0,0 +1,5 @@
+export const palette = {
+  white: "#fefefe",
+  black: "#2e2e2e",
+  blue: "#2a77f1",
+};
--- unistyles/themes.ts
+++ unistyles/themes.ts
@@ -1,7 +1,10 @@
+import { palette } from "./palette";
+
 export const lightTheme = {
   colors: {
-    typography: "#000000",
-    background: "#ffffff",
+    accent: palette.blue,
+    background: palette.white,
+    typography: palette.black,
   },
   margins: {
     sm: 2,
@@ -13,8 +16,9 @@ export const lightTheme = {
 
 export const darkTheme = {
   colors: {
-    typography: "#ffffff",
-    background: "#000000",
+    accent: palette.blue,
+    background: palette.black,
+    typography: palette.white,
   },
   margins: {
     sm: 2,
