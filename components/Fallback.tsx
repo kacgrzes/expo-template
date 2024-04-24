4b4454578d519a55ebc319b163c6b6fdf03cc33a
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-24 21:38:05 +0200

    Fix circular dependencies on Fallback component

--- components/Fallback.tsx
+++ components/Fallback.tsx
@@ -1,6 +1,8 @@
-import { Text, Button } from "components";
 import { FallbackProps } from "react-error-boundary";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Button } from "./Button";
+import { Text } from "./Text";
 
 export function Fallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
