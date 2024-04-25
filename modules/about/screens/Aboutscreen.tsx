e4bda63e23e2d0a07288d4b7825d0152d8887de1
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-25 23:51:18 +0200

    Move about screen to modals

--- modules/about/screens/Aboutscreen.tsx
+++ modules/about/screens/Aboutscreen.tsx
@@ -1,5 +1,22 @@
+import { ScrollView } from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
+
 import { Version } from "../components/Version";
 
 export function AboutScreen() {
-  return <Version />;
+  const { styles } = useStyles(stylesheet);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Version />
+    </ScrollView>
+  );
 }
+
+const stylesheet = createStyleSheet({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
