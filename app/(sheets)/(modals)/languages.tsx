bf0d5777160825b9f8f2772c75fa386e110f64b7
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-25 23:42:53 +0200

    Test BottomSheet in modal

--- app/(sheets)/(modals)/languages.tsx
+++ app/(sheets)/(modals)/languages.tsx
@@ -1,3 +1,13 @@
+import { Button } from "components";
+import { Link } from "expo-router";
+import { ScrollView } from "react-native";
+
 export default function Languages() {
-  return null;
+  return (
+    <ScrollView>
+      <Link href="example-sheet" asChild>
+        <Button title="Open example-sheet" variant="link" />
+      </Link>
+    </ScrollView>
+  );
 }
