ecf821bee5bad6c1f0e594ae4702e16984167959
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-04-28 22:00:39 +0200

    Improve JSONViewer

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,9 @@
 import { ExpoConfig, ConfigContext } from "expo/config";
-import { palette } from "unistyles/palette";
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   const defaultConfig = {
     ...config,
     slug: "template",
-    primaryColor: palette.blue,
   } as ExpoConfig;
 
   if (process.env.NODE_ENV === "development") {
--- components/JSONViewer.tsx
+++ components/JSONViewer.tsx
@@ -1,5 +1,7 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
+
+import { Text } from "./Text";
 
 type JSONViewerProps = {
   content: object;
@@ -17,10 +19,12 @@ export function JSONViewer({ content }: JSONViewerProps) {
   );
 }
 
-const stylesheet = createStyleSheet({
-  jsonViewer: {
-    padding: 16,
-    backgroundColor: "#e6e6e6",
-    borderRadius: 8,
-  },
+const stylesheet = createStyleSheet((theme, runtime) => {
+  return {
+    jsonViewer: {
+      padding: 16,
+      backgroundColor: runtime.colorScheme === "dark" ? "#202020" : "#e6e6e6",
+      borderRadius: 8,
+    },
+  };
 });
