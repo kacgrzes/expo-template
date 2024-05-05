dd871c5394a1882b2df60dff3e7ffd9ad6870e38
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-05-05 22:27:19 +0200

    Create RefreshControl component

---
+++ components/RefreshControl/RefreshControl.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useCallback, useState } from "react";
+import { ScrollView } from "react-native";
+
+import { RefreshControl } from "./RefreshControl";
+import { Text } from "../Text";
+
+const Component = () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const handleRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+      }>
+      <Text textAlign="center">Pull to refresh</Text>
+    </ScrollView>
+  );
+};
+
+const RefreshControlMeta: Meta<typeof RefreshControl> = {
+  title: "RefreshControl",
+  component: Component,
+};
+
+export default RefreshControlMeta;
+
+export const Default: StoryObj<typeof RefreshControl> = {};
---
+++ components/RefreshControl/RefreshControl.tsx
@@ -0,0 +1,23 @@
+import {
+  RefreshControl as RefreshControlComponent,
+  RefreshControlProps as RefreshControlComponentProps,
+} from "react-native";
+import { useStyles } from "react-native-unistyles";
+
+type RefreshControlProps = Pick<
+  RefreshControlComponentProps,
+  "refreshing" | "onRefresh"
+>;
+
+export function RefreshControl(props: RefreshControlProps) {
+  const { theme } = useStyles();
+
+  return (
+    <RefreshControlComponent
+      colors={[theme.colors.background]}
+      progressBackgroundColor={theme.colors.typography}
+      tintColor={theme.colors.accent}
+      {...props}
+    />
+  );
+}
