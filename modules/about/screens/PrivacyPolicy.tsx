aeed221599be448b23b9233f6ce99eb40cb6e902
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-05-05 21:43:31 +0200

    Make a draft for PrivacyPolicy and TermsAndConditions

---
+++ modules/about/screens/PrivacyPolicy.tsx
@@ -0,0 +1,10 @@
+import { Text } from "components";
+import { ScrollView } from "react-native";
+
+export function PrivacyPolicy() {
+  return (
+    <ScrollView>
+      <Text>Privacy Policy</Text>
+    </ScrollView>
+  );
+}
