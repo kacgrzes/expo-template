4e202bbeb5ce33aa7e9b4205726a370138ef5893
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-05-06 22:54:46 +0200

    Improve theme file, extract some common stuff

--- unistyles/themes.ts
+++ unistyles/themes.ts
@@ -1,30 +1,31 @@
+import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 import { palette } from "./palette";
 
+const common = {
+  margins: {
+    sm: 2,
+    md: 4,
+    lg: 8,
+    xl: 12,
+  },
+  opacity: 0.4,
+} as const;
+
 export const lightTheme = {
+  ...common,
   colors: {
     accent: palette.blue,
     background: palette.white,
     typography: palette.black,
   },
-  margins: {
-    sm: 2,
-    md: 4,
-    lg: 8,
-    xl: 12,
-  },
 } as const;
 
 export const darkTheme = {
+  ...common,
   colors: {
     accent: palette.blue,
     background: palette.black,
     typography: palette.white,
-  },
-  margins: {
-    sm: 2,
-    md: 4,
-    lg: 8,
-    xl: 12,
   },
 } as const;
 
