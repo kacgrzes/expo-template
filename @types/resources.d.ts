8f61e87cba2614074f1fba8376fe08ea16bc78ec
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-05-13 23:02:01 +0200

    Add some translations for i18n module; update fontFamily in modal stack

--- @types/resources.d.ts
+++ @types/resources.d.ts
@@ -1,7 +1,16 @@
 interface Resources {
-  common: {
-    "Welcome to React": "Welcome to React and react-i18next";
-  };
+  "en": {
+    "Welcome to React": "Welcome to React and react-i18next",
+    "languages": "Languages"
+  },
+  "fr": {
+    "Welcome to React": "Bienvenue à React et react-i18next",
+    "languages": "Langues"
+  },
+  "pl": {
+    "Welcome to React": "Witaj w React i react-i18next",
+    "languages": "Języki"
+  }
 }
 
 export default Resources;
--- app/(sheets)/(modals)/_layout.tsx
+++ app/(sheets)/(modals)/_layout.tsx
@@ -1,13 +1,47 @@
 import { Stack } from "expo-router";
+import { useTranslation } from "react-i18next";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 export default function Modals() {
+  const { styles, theme } = useStyles(stylesheet);
+  const { t } = useTranslation();
+
   return (
     <Stack
       screenOptions={{
-        header: () => null,
-        headerShown: false,
         presentation: "modal",
-      }}
-    />
+        headerStyle: styles.headerStyle,
+        headerTitleStyle: {
+          fontFamily: theme.fontFamily.IBMPlexSans_500Medium,
+        },
+      }}>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          header: () => null,
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{
+          title: "About",
+        }}
+      />
+      <Stack.Screen
+        name="languages"
+        options={{
+          title: t("languages"),
+        }}
+      />
+    </Stack>
   );
 }
+
+const stylesheet = createStyleSheet((theme) => {
+  return {
+    headerStyle: {
+      backgroundColor: theme.colors.background,
+    },
+  };
+});
