88f1b61caf10cd6a146372e7cb0b37315580940c
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-05-13 22:54:59 +0200

    Improve AboutScreen

--- modules/about/screens/AboutScreen/AboutScreen.tsx
+++ modules/about/screens/AboutScreen/AboutScreen.tsx
@@ -1,6 +1,7 @@
-import { ListItem } from "components";
+import { ListItem, SocialIcon, Title } from "components";
+import { applicationName } from "expo-application";
 import { useRouter } from "expo-router";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 import { Version } from "../../components/Version";
@@ -11,6 +12,9 @@ export function AboutScreen() {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.top}>
+        <Title>{applicationName}</Title>
+      </View>
       <ListItem
         icon="Clipboard"
         title="Terms and conditions"
@@ -24,15 +28,28 @@ export function AboutScreen() {
           router.navigate("/licenses");
         }}
       />
+      <View style={styles.socials}>
+        <SocialIcon name="Facebook" onPress={() => {}} />
+        <SocialIcon name="Instagram" onPress={() => {}} />
+        <SocialIcon name="Twitter" onPress={() => {}} />
+      </View>
       <Version />
     </ScrollView>
   );
 }
 
 const stylesheet = createStyleSheet({
+  top: {
+    padding: 24,
+  },
   container: {
     // flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  socials: {
+    flexDirection: "row",
+    gap: 8,
+    padding: 16,
   },
 });
