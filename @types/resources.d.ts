cb88f02388169f2a8043c5d432877e3de70bd962
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-05-15 21:51:15 +0200

    Fix some linting issues

--- @types/resources.d.ts
+++ @types/resources.d.ts
@@ -1,16 +1,16 @@
 interface Resources {
-  "en": {
-    "Welcome to React": "Welcome to React and react-i18next",
-    "languages": "Languages"
-  },
-  "fr": {
-    "Welcome to React": "Bienvenue à React et react-i18next",
-    "languages": "Langues"
-  },
-  "pl": {
-    "Welcome to React": "Witaj w React i react-i18next",
-    "languages": "Języki"
-  }
+  en: {
+    "Welcome to React": "Welcome to React and react-i18next";
+    languages: "Languages";
+  };
+  fr: {
+    "Welcome to React": "Bienvenue à React et react-i18next";
+    languages: "Langues";
+  };
+  pl: {
+    "Welcome to React": "Witaj w React i react-i18next";
+    languages: "Języki";
+  };
 }
 
 export default Resources;
