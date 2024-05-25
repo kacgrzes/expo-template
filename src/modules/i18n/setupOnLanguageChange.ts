59752e587a980314d3e53f9acd7d0100b89c6cf7
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-05-25 21:37:13 +0200

    Reduce bundle size by reducing imported locales

--- src/modules/i18n/setupOnLanguageChange.ts
+++ src/modules/i18n/setupOnLanguageChange.ts
@@ -1,5 +1,7 @@
 import { Locale, setDefaultOptions } from "date-fns";
-import { pl, fr, enGB as en } from "date-fns/locale";
+import { pl } from "date-fns/locale/pl";
+import { fr } from "date-fns/locale/fr";
+import { enGB as en } from "date-fns/locale/en-GB";
 import i18next from "i18next";
 
 const locales: Record<string, Locale> = { pl, fr, en };
