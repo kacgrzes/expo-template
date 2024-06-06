a482139ab34e0db1cf51f7695a41255d68cea0ee
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-06 18:59:00 +0200

    Install Skia

--- src/app/(sheets)/(modals)/(tabs)/(examples)/_layout.tsx
+++ src/app/(sheets)/(modals)/(tabs)/(examples)/_layout.tsx
@@ -4,7 +4,8 @@ import { SafeAreaView } from "components";
 export default function TopTabsExample() {
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
-      <TopTabs screenOptions={{}}>
+      <TopTabs
+        screenOptions={{ tabBarStyle: { backgroundColor: "transparent" } }}>
         <TopTabs.Screen name="tab1" options={{}} />
         <TopTabs.Screen name="tab2" options={{}} />
         <TopTabs.Screen name="tab3" options={{}} />
--- src/modules/unistyles/ThemeSwitcher.tsx
+++ src/modules/unistyles/ThemeSwitcher.tsx
@@ -1,4 +1,4 @@
-import { Button } from "components";
+import { Button } from "components/Button";
 
 import { useTheme } from "./ThemeProvider";
 
