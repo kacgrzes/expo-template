6051b10cc4d8e5ac9f1201ec59d40f17a1618672
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-07 19:06:38 +0200

    Disable CSS in metro.config.js, its not needed

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: false,
+});
 
 config.transformer.unstable_allowRequireContext = true;
 
