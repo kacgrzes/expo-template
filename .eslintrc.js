83d3cbdfe472156a112280e6340216ba5e94a7e5
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-07 19:25:46 +0200

    More configuration of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   root: true,
   extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["expo", "prettier"],
   rules: {
+    "expo/no-dynamic-env-var": "error",
+    "expo/no-env-var-destructuring": "error",
+    "prettier/prettier": ["error"],
     // Ensures props and state inside functions are always up-to-date
     "react-hooks/exhaustive-deps": "warn",
-    "prettier/prettier": ["error"],
   },
 };
