873bf918c81b2f1153d2846bb0a06d047baeb547
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-08 13:23:07 +0200

    Add newArchEnabled set to false; remove console.log from production environment

--- metro.config.js
+++ metro.config.js
@@ -9,4 +9,11 @@ const config = getDefaultConfig(__dirname, {
 
 config.transformer.unstable_allowRequireContext = true;
 
+config.transformer.minifierConfig = {
+  compress: {
+    // The option below removes all console logs statements in production.
+    drop_console: true,
+  },
+};
+
 module.exports = config;
