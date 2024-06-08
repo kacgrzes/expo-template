642db8aa5a9c5dd9b0fe1bd9272f705365d16dd7
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-08 10:25:57 +0200

    Setup multiple app variants

--- app.config.ts
+++ app.config.ts
@@ -1,20 +1,50 @@
 import { ExpoConfig, ConfigContext } from "expo/config";
 
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const extendUniqueIdentifier = (identifier: string) => {
+  if (IS_DEV) {
+    return `${identifier}.dev`;
+  }
+
+  if (IS_PREVIEW) {
+    return `${identifier}.preview`;
+  }
+
+  return identifier;
+};
+
+const extendAppName = (originalAppName: string) => {
+  if (IS_DEV) {
+    return `${originalAppName} (Dev)`;
+  }
+
+  if (IS_PREVIEW) {
+    return `${originalAppName} (Preview)`;
+  }
+
+  return originalAppName;
+};
+
 export default ({ config }: ConfigContext): ExpoConfig => {
   const defaultConfig = {
     ...config,
     slug: "template",
   } as ExpoConfig;
 
-  if (process.env.NODE_ENV === "development") {
-    return {
-      ...defaultConfig,
-      name: "Template (development)",
-    };
-  }
-
   return {
     ...defaultConfig,
-    name: "Template",
+    name: extendAppName(defaultConfig.name),
+    ios: {
+      ...defaultConfig.ios,
+      bundleIdentifier: extendUniqueIdentifier(
+        defaultConfig.ios?.bundleIdentifier!,
+      ),
+    },
+    android: {
+      ...defaultConfig.android,
+      package: extendUniqueIdentifier(defaultConfig.android?.package!),
+    },
   };
 };
--- src/hooks/useQuickActionSetup.ts
+++ src/hooks/useQuickActionSetup.ts
@@ -2,10 +2,18 @@ import * as QuickActions from "expo-quick-actions";
 import { useQuickActionRouting } from "expo-quick-actions/router";
 import { useEffect } from "react";
 
+/**
+ * iOS best practices:
+ * @see https://github.com/EvanBacon/expo-quick-actions?tab=readme-ov-file#ios-best-practice
+ *
+ * Android best practices:
+ * @see https://github.com/EvanBacon/expo-quick-actions?tab=readme-ov-file#android-best-practice
+ */
 export const useQuickActionSetup = () => {
   useQuickActionRouting();
   useEffect(() => {
     // Use all 4 slots
+    // Avoid branded icons
     QuickActions.setItems([
       {
         title: "Wait! Don't delete!",
