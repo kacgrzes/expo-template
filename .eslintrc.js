38cca434b40e8948ae0121a90adfa89d42bab477
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-09 11:54:40 +0200

    Update package.json, tsconfig.json, .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   root: true,
   extends: ["expo", "prettier"],
-  plugins: ["expo", "prettier"],
+  plugins: ["react-native-globals", "expo", "prettier"],
   rules: {
     "expo/no-dynamic-env-var": "error",
     "expo/no-env-var-destructuring": "error",
     "prettier/prettier": ["error"],
     // Ensures props and state inside functions are always up-to-date
     "react-hooks/exhaustive-deps": "warn",
+  },
+  env: {
+    "react-native-globals/all": true,
   },
 };
