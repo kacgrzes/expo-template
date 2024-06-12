6838f724cbb7633cd36631218c26df46f510a4d7
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-12 19:55:55 +0200

    Add polyfill for console.time

--- src/components/ScrollView/ScrollView.tsx
+++ src/components/ScrollView/ScrollView.tsx
@@ -1,5 +1,5 @@
 import { NativeScrollEvent, NativeSyntheticEvent } from "react-native";
 
-export { ScrollView } from "react-native";
+export { ScrollView } from "react-native-gesture-handler";
 
 export type OnScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => void;
