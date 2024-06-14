3d6ac62a100e14b9dc69c2045375860f5b84cad3
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-14 09:34:50 +0200

    Mock async-storage

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,3 +5,6 @@ jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 jest.mock("react-native-keyboard-controller", () =>
   require("react-native-keyboard-controller/jest"),
 );
+jest.mock("@react-native-async-storage/async-storage", () =>
+  require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
+);
