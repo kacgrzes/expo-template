d768a0998bbda808916b88de8b92f7a90ba7580d
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-06-15 20:37:36 +0200

    Improve jest config with ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,14 @@
 import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+import tsConfig from "./tsconfig.json";
 
 const config: Config = {
+  moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths),
+  modulePaths: [tsConfig.compilerOptions.baseUrl],
   preset: "jest-expo",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["<rootDir>/src/**/*.test.ts", "<rootDir>/src/**/*.test.tsx"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
