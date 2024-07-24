d606a7893750812d0bdead1a4cd74185be162569
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-07-24 17:26:34 +0200

    Format Screen components

--- packages/@mobile/components/src/Screen/Screen.tsx
+++ packages/@mobile/components/src/Screen/Screen.tsx
@@ -1,5 +1,6 @@
 import { Box, BoxProps } from "@grapp/stacks";
 import { useLayout } from "@react-native-community/hooks";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import React, { ReactElement, useMemo, MemoExoticComponent } from "react";
 import { KeyboardStickyView } from "react-native-keyboard-controller";
 import {
