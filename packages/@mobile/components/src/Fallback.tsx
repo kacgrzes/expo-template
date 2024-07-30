55eb20fb9489ddc410dbf87b598cd24f30543aec
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-07-30 20:45:15 +0200

    =Improve Fallback component styling

--- packages/@mobile/components/src/Fallback.tsx
+++ packages/@mobile/components/src/Fallback.tsx
@@ -1,15 +1,27 @@
 import { FallbackProps } from "react-error-boundary";
 
+import { Box } from "@grapp/stacks";
 import { Button } from "./Button";
-import { SafeAreaView } from "./SafeAreaView";
+import { JSONViewer } from "./JSONViewer";
+import { Screen } from "./Screen";
 import { Text } from "./Text";
 
 export function Fallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
-    <SafeAreaView>
-      <Text>Something went wrong!</Text>
-      <Text>{error.message}</Text>
-      <Button title="Try again" onPress={resetErrorBoundary} />
-    </SafeAreaView>
+    <Screen
+      edges={["bottom", "top"]}
+      footer={
+        <Box padding={4}>
+          <Button title="Try again" onPress={resetErrorBoundary} />
+        </Box>
+      }
+    >
+      <Screen.ScrollView>
+        <Box gap={4}>
+          <Text>Something went wrong!</Text>
+          <JSONViewer content={error.message} />
+        </Box>
+      </Screen.ScrollView>
+    </Screen>
   );
 }
