03d5f3c45d9b1eb36388d97325568715fc597865
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-08-09 22:51:56 +0200

    Draft some work on SegmentedControl

--- packages/@mobile/components/src/SegmentedControl/SegmentedControl.tsx
+++ packages/@mobile/components/src/SegmentedControl/SegmentedControl.tsx
@@ -4,16 +4,133 @@ import { LayoutChangeEvent, LayoutRectangle, View } from "react-native";
 import Animated, {
   useAnimatedStyle,
   withTiming,
+  Easing,
 } from "react-native-reanimated";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { AnimatedRectButton } from "../AnimatedButtons";
 import { Text } from "../Text";
 import { useDisabledStyle } from "../hooks/useDisabledStyle";
+import { Size } from "../types";
 
-type SegmentedControlProps = { disabled?: boolean; full?: boolean };
+type SegmentedControlProps = {
+  disabled?: boolean;
+  full?: boolean;
+  onValueChange: (index: number) => void;
+  selectedIndex?: number;
+  defaultSelectedIndex?: number;
+  size: Size;
+  // animated?: boolean;
+  // multiselect?: boolean;
+  // testID?: string;
+  // onChange (with event)
+  // withItemBorders
+};
+
+// TODO
+// accessibility!!!
+// Swipeable?
+// interesting scrollable segments - https://ionicframework.com/docs/api/segment
+// SegmentedControl.Root
+// SegmentedControl.Item (disabled, value)
+// SegmentedControl.Icon
+// SegmentedControl.Label
+// SegmentedControl.Indicator
+// can I do some maskview magic to make the active segment look like it's sliding?
 
 const BORDER_SIZE = 2;
 const BORDER_RADIUS = 8;
+
+const useSegmentedControl = () => {
+  const [measurements, setMeasurements] = useState<LayoutRectangle[]>([]);
+
+  const value = useMemo(
+    () => ({
+      measurements,
+      numberOfMeasurements: measurements.length,
+      setMeasurements,
+    }),
+    [measurements],
+  );
+
+  return value;
+};
+
+const [SegmentedControlProvider, useSegmentedControlContext] =
+  constate(useSegmentedControl);
+
+/**
+ * - Usually used for selecting between a small set of mutually exclusive options, often in a more compact space.
+ * - Best suited for a small number of options (typically 2-5) due to space constraints.
+ * - Typically used to switch between different modes or filter options within the same view.
+ */
+export function SegmentedControl({ disabled, full }: SegmentedControlProps) {
+  const { styles } = useStyles(stylesheet);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const disabledStyle = useDisabledStyle({ disabled });
+
+  return (
+    <SegmentedControlProvider>
+      <Animated.View style={[styles.track, disabledStyle]}>
+        <Segment
+          active={selectedIndex === 0}
+          disabled={disabled}
+          full={full}
+          index={0}
+          label="Segment #1"
+          onPress={() => setSelectedIndex(0)}
+        />
+        <Segment
+          active={selectedIndex === 1}
+          disabled={disabled}
+          full={full}
+          index={1}
+          label="Segment #2"
+          onPress={() => setSelectedIndex(1)}
+        />
+        <Segment
+          active={selectedIndex === 2}
+          disabled={disabled}
+          full={full}
+          index={2}
+          label="Segment #3 very very long text"
+          onPress={() => setSelectedIndex(2)}
+        />
+        <SegmentBackground selectedIndex={selectedIndex} full={full} />
+      </Animated.View>
+    </SegmentedControlProvider>
+  );
+}
+
+const stylesheet = createStyleSheet((theme) => {
+  return {
+    track: {
+      flexDirection: "row",
+      backgroundColor: theme.name === "dark" ? "grey" : "lightgrey",
+      padding: BORDER_SIZE,
+      borderRadius: BORDER_RADIUS,
+      gap: BORDER_SIZE,
+      height: 44,
+    },
+    segment: (full?: boolean, active?: boolean) => {
+      return {
+        height: "100%",
+        borderRadius: BORDER_RADIUS - BORDER_SIZE,
+        flexGrow: full ? 1 : undefined,
+        backgroundColor: active ? theme.colors.background : undefined,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: BORDER_RADIUS - BORDER_SIZE,
+      };
+    },
+    activeSegment: {
+      backgroundColor: theme.colors.background,
+      borderRadius: BORDER_RADIUS - BORDER_SIZE,
+      position: "absolute",
+      top: BORDER_SIZE,
+      zIndex: -1,
+    },
+  };
+});
 
 function Segment({
   active,
@@ -53,29 +170,13 @@ function Segment({
         onPress={onPress}
         style={styles.segment(full, active && numberOfMeasurements !== 3)}
       >
-        <Text variant="label1">{label}</Text>
+        <Text variant="label1" numberOfLines={1}>
+          {label}
+        </Text>
       </AnimatedRectButton>
     </View>
   );
 }
-
-const useSegmentedControl = () => {
-  const [measurements, setMeasurements] = useState<LayoutRectangle[]>([]);
-
-  const value = useMemo(
-    () => ({
-      measurements,
-      numberOfMeasurements: measurements.length,
-      setMeasurements,
-    }),
-    [measurements],
-  );
-
-  return value;
-};
-
-const [SegmentedControlProvider, useSegmentedControlContext] =
-  constate(useSegmentedControl);
 
 const SegmentBackground = ({
   selectedIndex,
@@ -97,12 +198,18 @@ const SegmentBackground = ({
     }
 
     return {
-      width: withTiming(measurement?.width, { duration: 300 }),
+      width: withTiming(measurement?.width, {
+        duration: 200,
+        easing: Easing.ease,
+      }),
       height: withTiming(measurement?.height),
       opacity: 1,
       transform: [
         {
-          translateX: withTiming(measurement?.x, { duration: 300 }),
+          translateX: withTiming(measurement?.x, {
+            duration: 200,
+            easing: Easing.ease,
+          }),
         },
       ],
     };
@@ -110,72 +217,3 @@ const SegmentBackground = ({
 
   return <Animated.View style={[styles.activeSegment, animatedStyle]} />;
 };
-
-export function SegmentedControl({ disabled, full }: SegmentedControlProps) {
-  const { styles } = useStyles(stylesheet);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const disabledStyle = useDisabledStyle({ disabled });
-
-  return (
-    <SegmentedControlProvider>
-      <Animated.View style={[styles.track, disabledStyle]}>
-        <Segment
-          active={selectedIndex === 0}
-          disabled={disabled}
-          full={full}
-          index={0}
-          label="Segment #1"
-          onPress={() => setSelectedIndex(0)}
-        />
-        <Segment
-          active={selectedIndex === 1}
-          disabled={disabled}
-          full={full}
-          index={1}
-          label="Segment #2"
-          onPress={() => setSelectedIndex(1)}
-        />
-        <Segment
-          active={selectedIndex === 2}
-          disabled={disabled}
-          full={full}
-          index={2}
-          label="Segment #3"
-          onPress={() => setSelectedIndex(2)}
-        />
-        <SegmentBackground selectedIndex={selectedIndex} full={full} />
-      </Animated.View>
-    </SegmentedControlProvider>
-  );
-}
-
-const stylesheet = createStyleSheet((theme) => {
-  return {
-    track: {
-      flexDirection: "row",
-      backgroundColor: theme.name === "dark" ? "grey" : "lightgrey",
-      padding: BORDER_SIZE,
-      borderRadius: BORDER_RADIUS,
-      gap: BORDER_SIZE,
-      height: 44,
-    },
-    segment: (full?: boolean, active?: boolean) => {
-      return {
-        height: "100%",
-        borderRadius: BORDER_RADIUS - BORDER_SIZE,
-        flexGrow: full ? 1 : undefined,
-        backgroundColor: active ? theme.colors.background : undefined,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingHorizontal: BORDER_RADIUS - BORDER_SIZE,
-      };
-    },
-    activeSegment: {
-      backgroundColor: theme.colors.background,
-      borderRadius: BORDER_RADIUS - BORDER_SIZE,
-      position: "absolute",
-      top: BORDER_SIZE,
-      zIndex: -1,
-    },
-  };
-});
