e49d01a5375ec0adc9627fe47a3f7a082e93d519
Kacper Grzeszczyk <kacgrzes@gmail.com>
2024-08-16 08:53:28 +0200

    Draft Stepper API

---
+++ packages/@mobile/components/src/Stepper/index.ts
@@ -0,0 +1,23 @@
+// ref - reset, set, increment, decrement
+
+// <Stepper.Root
+// ref
+// accessibilityLabel
+// testID
+// min
+// max
+// onChange
+// onValueChange
+// initialValue
+// defaultValue
+// value
+// orientation
+// step
+// disabled
+// size
+// >
+// <Stepper.Decrement />
+// <Stepper.Display /> // render prop <Stepper.Value />
+// <Stepper.Increment />
+// <Stepper.Icon />
+// </Stepper.Root>;
